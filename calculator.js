ea152bf22ae4c53d2fca8e2b35922a13c3191028
Sarah Hudson <78184189+sarahhcodes@users.noreply.github.com>
2023-08-15 17:19:37 -0700

    Created JS operators & HTML calculator
    The calculator is not functional yet in the HTML, but the operators work in the console.

---
+++ calculator.js
@@ -0,0 +1,31 @@
+let firstNumber, secondNumber, operator;
+
+function add(x,y) {
+    return x + y;
+};
+
+function subtract(x,y) {
+    return x - y;
+};
+
+function multiply(x,y) {
+    return x * y;
+};
+
+function divide(x,y) {
+    return x / y;
+};
+
+function operate(operator, x, y) {
+    if (operator == 'add') {
+        return add(x,y);
+    } else if (operator == 'subtract') {
+        return subtract(x,y);
+    } else if (operator == 'multiply') {
+        return multiply(x,y);
+    } else if (operator == 'divide') {
+        return divide(x,y);
+    } else {
+        return 'ERROR';
+    };
+};
