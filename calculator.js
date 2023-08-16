43db5b312e7b9b6bf671e8b83b68438adb3f28dd
Sarah Hudson <78184189+sarahhcodes@users.noreply.github.com>
2023-08-16 14:26:21 -0700

    Added functionality to calculator

--- calculator.js
+++ calculator.js
@@ -1,13 +1,16 @@
-let firstNumber, secondNumber, operator;
-let displayValue;
+let firstNumber = undefined;
+let secondNumber = undefined;
+let operator = undefined;
+let input;
+let dot = false;
+
 const buttons = document.querySelectorAll('button');
 const display = document.querySelector('.display');
 const results = document.querySelector('.results');
 
 buttons.forEach(button => button.addEventListener('click', () => {
-    displayValue = button.className;
-    results.textContent = displayValue;
-    display.appendChild(results);
+    input = button.className;
+    evaluateInput(input);
 }));
 
 function add(x,y) {
@@ -23,7 +26,11 @@ function multiply(x,y) {
 };
 
 function divide(x,y) {
-    return x / y;
+    if (y !== 0) {
+        return x / y;
+    } else {
+        return "FORBIDDEN!"
+    }
 };
 
 function operate(operator, x, y) {
@@ -39,3 +46,63 @@ function operate(operator, x, y) {
         return 'ERROR';
     };
 };
+
+function evaluateInput(input) {
+    if (input === 'add' || input === 'subtract' || input === 'multiply' || 
+        input === 'divide') {
+        operator = input;
+
+        if (firstNumber !== undefined && secondNumber !== undefined) {
+            firstNumber = operate(operator, firstNumber, secondNumber);
+            secondNumber = undefined;
+            results.textContent = firstNumber;
+        } else {
+            return;
+        };
+    } else if (input === "clear") {
+        firstNumber = undefined;
+        secondNumber = undefined;
+        operator = undefined;
+        results.textContent = "HELLO";
+    } else if (input === "equals") {
+        if (firstNumber === undefined || secondNumber === undefined || operator === undefined) {
+            return; // do nothing
+        } else { 
+            firstNumber = operate(operator, firstNumber, secondNumber);
+            if (firstNumber%1 !== 0) {
+                firstNumber = Number((firstNumber).toFixed(5));
+            }
+            secondNumber = undefined;
+            operator = undefined;
+            results.textContent = firstNumber;
+        };
+    } 
+    else if (firstNumber !== undefined && !dot && input === "dot") {
+        dot = true;
+        firstNumber += ".";
+        results.textContent = firstNumber;
+    } else if (dot) {
+        // logic for handling floats
+    } else {
+        if (firstNumber === undefined) {
+            input = parseInt(input);
+            firstNumber = input;
+        } else if (operator === undefined) { // selecting numbers greater than 9
+            firstNumber = String(firstNumber);
+            firstNumber = firstNumber + input;
+            firstNumber = parseInt(firstNumber);
+            input = firstNumber;
+        } else if (secondNumber !== undefined) {
+            secondNumber = String(secondNumber);
+            secondNumber = secondNumber + input;
+            secondNumber = parseInt(secondNumber);
+            input = secondNumber;
+        } else {
+            input = parseInt(input);
+            secondNumber = input;
+        };
+        results.textContent = input;
+    } 
+
+    display.appendChild(results);
+}
